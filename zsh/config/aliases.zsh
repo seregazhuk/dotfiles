d7dc7b914cecea2091e78eb02d9573cffe5b84af
Zhuk Sergey <seregazhuk88@gmail.com>
2016-02-13 21:52:59 +0300

    zsh added

---
+++ zsh/config/aliases.zsh
@@ -0,0 +1,12 @@
+# Git Stuff
+# -----------------------------------------------------------------------------
+alias ga="git add"
+alias gaa="git add ."
+alias gc="git commit -m "
+alias gs="git status"
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+# Laravel
+# -----------------------------------------------------------------------------
+alias tinker="php artisan tinker"
+alias serve="php artisan serve"
