ff1f55357605dbd4ca4107de103252a78d09b846
Zhuk Sergey <seregazhuk88@gmail.com>
2016-04-09 08:50:34 +0300

    brew heroku add

--- brew/brew.sh
+++ brew/brew.sh
@@ -10,22 +10,17 @@ brew upgrade
 # GNU core utilities (those that come with OS X are outdated)
 brew install coreutils
 
-# Bash 4
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before running `chsh`.
-brew install bash
 brew tap homebrew/versions
-brew install bash-completion2
+
 brew install mongo
 brew install homebrew/completions/brew-cask-completion
+
+
 # Install wget with IRI support
 brew install wget --with-iri
 
 # Install more recent versions of some OS X tools
 brew install macvim --override-system-vim
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
-
 
 # github util. imho better than hub
 brew install gh
@@ -39,6 +34,8 @@ brew install ffmpeg --with-libvpx
 brew install terminal-notifier
 brew install zsh
 brew install http
+brew install heroku-toolbelt
+brew install htop
 
 brew linkapps
 # Remove outdated versions from the cellar
