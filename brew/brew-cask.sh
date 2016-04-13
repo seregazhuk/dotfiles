a3944f1f43beb6b000726e8167664a4aa591f775
Zhuk Sergey <seregazhuk88@gmail.com>
2016-04-13 07:27:25 +0300

    brew config update

--- brew/brew-cask.sh
+++ brew/brew-cask.sh
@@ -10,11 +10,12 @@ brew cask install dropbox
 brew cask install 1password
 brew cask install telegram
 brew cask install skype
+brew cask install slack
 
 # dev
 brew cask install iterm2
 brew cask install sublime-text
-brew cask install slack
+brew cask install phpstorm
 brew cask install virtualbox
 brew cask install filezilla
 brew cask install vagrant
