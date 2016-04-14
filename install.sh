f4703e494dbff3f81f109c683e4f3f1abd0e0c3c
Zhuk Sergey <seregazhuk88@gmail.com>
2016-04-14 07:29:56 +0300

    Brewfile

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo "Setting up your Mac..."
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle
+
+# Install Composer
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+
+# Install global Composer packages
+/usr/local/bin/composer global require laravel/installer phpunit/phpunit
+
+# Set OS X preferences
+# Run this last because this will reload the shell
+source .osx
