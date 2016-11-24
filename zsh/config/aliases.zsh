3273f2f4c23231f8c896dd5376efa9aee691b461
Zhuk Sergey <seregazhuk88@gmail.com>
2016-11-24 22:24:13 +0300

    git aliases update

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -4,6 +4,7 @@ alias ga="git add"
 alias gaa="git add ."
 alias gc="git commit -m "
 alias gs="git status"
+alias gp="git push"
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
 # PHP
