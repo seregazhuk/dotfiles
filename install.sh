04002b870c69a76dbac3f713fd7c845731190cc7
Sergey Zhuk <seregazhuk88@gmail.com>
2017-06-21 00:25:06 +0300

    add: brew link applications

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ brew update
 # Install all our dependencies with bundle (See Brewfile)
 brew tap homebrew/bundle
 brew bundle
+brew linkapps
 
 # Install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
