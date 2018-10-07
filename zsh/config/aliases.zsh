a17d77d190b84b22a467a82bc32ac87616191473
Zhuk Sergey <seregazhuk88@gmail.com>
2018-10-07 15:00:14 +0300

    remove blog alias

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -48,5 +48,4 @@ alias sudo='sudo '
 # Misc
 # -----------------------------------------------------------------------------
 alias weather='curl -s wttr.in | sed -n "1,7p"'
-alias blog='cd /Users/serega/Develop/seregazhuk.github.io && subl . && jekyll serve'
 alias ar='sudo /usr/sbin/httpd -k restart'
